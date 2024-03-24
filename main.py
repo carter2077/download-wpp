7e52dc217e313983690daa43ae4ce9c71cdb438f
carter2077 <carter2077@pm.me>
2024-03-23 20:13:24 -0400

    First working version

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Firefox()
 driver.get('https://watchmanprivacy.libsyn.com/')
 
+# let the page load
 driver.implicitly_wait(5)
 
-ids = driver.find_elements(by=By.XPATH, value='//*[@id]')
-for ii in ids:
-    #print ii.tag_name
-    print(ii.get_attribute('id'))
-
-# get all the embed ids, scrolling down the page
+# scroll all the way down the page so we can get all embeds
 # infinite scroll... shout out to the below link
 # https://stackoverflow.com/questions/20986631/how-can-i-scroll-a-web-page-using-selenium-webdriver-in-python
 SCROLL_PAUSE_TIME = 0.5
@@ -33,8 +30,23 @@ while True:
         break
     last_height = new_height
 
+# get all embeds
+embeds = driver.find_elements(by=By.CSS_SELECTOR, value="[id^=embed_]")
+
 # for each embed id, grab the source and strip the html://// from the beginning
+original_window = driver.current_window_handle
+for e in embeds:
+    url = e.get_attribute("src")
+    # open the source in a new tab
+    driver.switch_to.new_window('tab')
+    print(url)
+    driver.get(url)
+    # click the download button, close the window
+    driver.implicitly_wait(0.5)
+    dl_btn = driver.find_element(by=By.ID, value="download-player")
+    dl_btn.click()
+    driver.implicitly_wait(1)
+    driver.close()
+    driver.switch_to.window(original_window)
 
-## open the source in a new window
 
-## click the download button, close the window
